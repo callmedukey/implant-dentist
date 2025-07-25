32be35a5ead2b477d4b7d329c508c53c64b51bf1
callmedukey <87065409+callmedukey@users.noreply.github.com>
2025-07-25 09:53:01 +0900

    abc

--- app/api/popups/route.ts
+++ app/api/popups/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 
 import { prisma } from "@/prisma/prisma-client";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const popups = await prisma.popup.findMany({
--- app/api/uploads/[filename]/route.ts
+++ app/api/uploads/[filename]/route.ts
@@ -3,6 +3,8 @@ import path from "path";
 
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ filename: string }> }
